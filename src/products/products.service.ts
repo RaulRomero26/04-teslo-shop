ebd6164812e05bf55ddcf1059f525a48970c354c
RaulRomero26 <link_2642@hotmail.com>
2024-08-05 16:48:07 -0600

    update: find all, find one, delete one

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
@@ -31,20 +31,35 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    const products = this.productRepository.find();
+    return products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+
+      const product = await this.productRepository.findOneBy({
+        id:id
+      });
+      if(!product){
+        throw new NotFoundException(`Product with ${id} not found`);
+      }
+      return product;
+
+    
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    
+    const product = await this.findOne(id);
+    if(!product){
+      throw new NotFoundException(`Product with ${id} not found`);
+    }
+    await this.productRepository.remove(product); 
   }
 
   private handleDBExceptions(error:any){
