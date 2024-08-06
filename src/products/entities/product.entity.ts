f195d09251c0fff80e4de93a986efa684ca753cf
RaulRomero26 <link_2642@hotmail.com>
2024-08-06 10:55:01 -0600

    update product

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 
 @Entity()
@@ -56,5 +56,11 @@ export class Product {
             .replaceAll("'",'');
     }
 
-    // @BeforeUpdate()
+    @BeforeUpdate()
+    checkSlugUpdate(){
+        this.slug = this.slug
+            .toLowerCase()
+            .replaceAll(' ','_')
+            .replaceAll("'",'');
+    }
 }
