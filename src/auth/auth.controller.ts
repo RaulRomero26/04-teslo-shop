b5d7eea27a81171b8522c017f638c5bc994a7367
RaulRomero26 <link_2642@hotmail.com>
2024-08-13 17:15:54 -0600

    update: id en payload del strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -13,9 +14,18 @@ export class AuthController {
 
   @Post('login')//comentaroo
   loginUser(@Body() loginUserDto: LoginUserDto) {
-    console.log('Login endpoint hit'); // Agrega este console.log
-    console.log('Request body:', loginUserDto); // Agrega este console.log
+    console.log('Login endpoint hit');
+    console.log('Request body:', loginUserDto); 
     return  this.authService.login(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards(AuthGuard())
+  testigPrivateRoute() {
+    return{
+      ok: true,
+      message: 'Hola mundo private'
+    }
   }
 
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -27,7 +27,7 @@ export class AuthService {
 
       return {
         ...user,
-        token: this.getJwtToken({ email: user.email }),
+        token: this.getJwtToken({ id: user.id }),
       };
     } catch (error) {
       this.handleDBError(error);
@@ -41,7 +41,7 @@ export class AuthService {
 
       const user = await this.userRepository.findOne({
         where: { email },
-        select: { email: true, password: true },
+        select: { email: true, password: true, id: true },
       });
 
       if (!user) {
@@ -55,7 +55,7 @@ export class AuthService {
 
       return {
         ...user,
-        token: this.getJwtToken({ email: user.email }),
+        token: this.getJwtToken({ id: user.id }),
       };
     } catch (error) {
       this.handleDBError(error);
