e37b6582b5db1e8e5e8d6d920b92ab61e4c0073e
Raul Romero <link_2642@hotmail.com>
2024-08-19 12:03:01 -0600

    update: todo fix seed

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,8 +3,9 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
-import { Auth } from '../auth/decorators';
+import { Auth, GetUser } from '../auth/decorators';
 import { validRoles } from 'src/auth/interfaces';
+import { User } from 'src/auth/entities/user.entity';
 
 @Controller('products')
 //@Auth() //cualquiera de estas rutas debe de estar autenticado
@@ -12,9 +13,12 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  @Auth(validRoles.admin)
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  @Auth()
+  create(
+    @Body() createProductDto: CreateProductDto,
+    @GetUser() user: User
+  ) {
+    return this.productsService.create(createProductDto,user);
   }
 
   @Get()
@@ -28,11 +32,12 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  @Auth(validRoles.admin)
+  @Auth()
   update(
     @Param('id', ParseUUIDPipe) id: string, 
-    @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(id, updateProductDto);
+    @Body() updateProductDto: UpdateProductDto,
+    @GetUser() user: User) {
+    return this.productsService.update(id, updateProductDto, user);
   }
 
   @Delete(':id')
